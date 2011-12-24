f1d757b7dbf46fcb73d9c2952a6c34706e3b7f87
whtsky <whtsky@vip.qq.com>
2011-12-24 23:46:18 +0800

    精简代码

--- GoL.py
+++ GoL.py
@@ -13,7 +13,7 @@ else:
         os.system("clear")
 
 SIZE = 30 #表的边长.
-RANDOM = 10 #值越大初始细胞越多
+RANDOM = 20 #值越大初始细胞越多
 DAILY = 0.5 #演化间隔时间
 
 REALIVE = 3 #周围细胞数等于本数，该细胞复活
@@ -24,45 +24,31 @@ MIN = 1 #周围细胞数小于等于本数，该细胞死亡
 ALIVE = 1
 DEAD = 0
 
-table = []
-#创建细胞表
-for x in range(SIZE):
-    row = []
-    for y in range(SIZE):
-        row.append(DEAD)
-    table.append(row)
 #生成随机的存活细胞
-for x in range(SIZE):
-    for y in range(SIZE):
-        table[x][y] = ALIVE if randint(0,100) <= RANDOM else DEAD
-
+table = [([(ALIVE if randint(0,100) <= RANDOM else DEAD) for x in range(SIZE)]) for x in range(SIZE)]
+    
 def next():
     for x in range(SIZE):
         for y in range(SIZE):
             cell = table[x][y]
-            livenum = 0
-            livenum += table[(x + 1) % SIZE][(y - 1) % SIZE]
-            livenum += table[(x + 1) % SIZE][y]
-            livenum += table[(x + 1) % SIZE][(y + 1) % SIZE]
-            livenum += table[x][(y - 1) % SIZE]
-            livenum += table[x][(y + 1) % SIZE]
-            livenum += table[(x - 1) % SIZE][(y - 1) % SIZE]
-            livenum += table[(x - 1) % SIZE][y]
-            livenum += table[(x - 1) % SIZE][(y + 1) % SIZE]
+            livenum = -cell
+            for m in (x+1,x,x-1):
+                for n in (y-1,y,y+1):
+                    livenum += table[m % SIZE][n % SIZE]
 
-            if cell is ALIVE:
-                if livenum <= MIN or livenum >= MAX:
-                    table[x][y] = DEAD
+            if cell is ALIVE and (livenum <= MIN or livenum >= MAX):
+                table[x][y] = DEAD
             elif livenum == REALIVE:
                 table[x][y] = ALIVE
 
 def show():
-    result = ''
     for x in range(SIZE):
         for y in range(SIZE):
-            result += 'O ' if table[x][y] is ALIVE else '  '
-        result += '\n'
-    print result
+            if table[x][y] is ALIVE:
+                print 'O',
+            else:
+                print ' ',
+        print ''
 
 while True:
     cls()
