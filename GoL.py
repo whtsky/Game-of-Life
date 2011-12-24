33580ba1e84a3834d285626ee766d1969f52fb8e
whtsky <whtsky@vip.qq.com>
2011-12-25 01:28:22 +0800

    Use xrange() to save memory.

--- GoL.py
+++ GoL.py
@@ -4,15 +4,11 @@ from random import randint
 import os
 import platform
 from time import sleep
+cls = lambda :os.system("cls") if platform.system() == 'Windows' else os.system("clear")
+ALIVE = 1
+DEAD = 0
 
-if platform.system() == 'Windows':
-    def cls():
-        os.system("cls")
-else:
-    def cls():
-        os.system("clear")
-
-SIZE = 30 #表的边长.
+SIZE = 30 #表的长.
 RANDOM = 20 #值越大初始细胞越多
 DAILY = 0.5 #演化间隔时间
 
@@ -20,16 +16,12 @@ REALIVE = 3 #周围细胞数等于本数，该细胞复活
 MAX = 4 #周围细胞数大于等于本数，该细胞死亡
 MIN = 1 #周围细胞数小于等于本数，该细胞死亡
 
-
-ALIVE = 1
-DEAD = 0
-
 #生成随机的存活细胞
-table = [([(ALIVE if randint(0,100) <= RANDOM else DEAD) for x in range(SIZE)]) for x in range(SIZE)]
-    
+table = [([(ALIVE if randint(0,100) <= RANDOM else DEAD) for x in xrange(SIZE)]) for x in xrange(SIZE)]
+
 def next():
-    for x in range(SIZE):
-        for y in range(SIZE):
+    for x in xrange(SIZE):
+        for y in xrange(SIZE):
             cell = table[x][y]
             livenum = -cell
             for m in (x+1,x,x-1):
@@ -42,8 +34,8 @@ def next():
                 table[x][y] = ALIVE
 
 def show():
-    for x in range(SIZE):
-        for y in range(SIZE):
+    for x in xrange(SIZE):
+        for y in xrange(SIZE):
             if table[x][y] is ALIVE:
                 print 'O',
             else:
