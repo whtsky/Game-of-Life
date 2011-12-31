c5dbe154728210a904f99284d2d96c48a4a5d9dd
whtsky <whtsky@vip.qq.com>
2011-12-31 17:30:59 +0800

    Use assert.

--- GoL.py
+++ GoL.py
@@ -9,12 +9,14 @@ ALIVE = 1
 DEAD = 0
 
 SIZE = 30 #表的长.
-RANDOM = 20 #值越大初始细胞越多
+RANDOM = 50 #值越大初始细胞越多
 DAILY = 0.5 #演化间隔时间
 
 REALIVE = 3 #周围细胞数等于本数，该细胞复活
 MAX = 4 #周围细胞数大于等于本数，该细胞死亡
 MIN = 1 #周围细胞数小于等于本数，该细胞死亡
+
+assert 8>=MAX>=REALIVE>=MIN>=0
 
 #生成随机的存活细胞
 table = [([(ALIVE if randint(0,100) <= RANDOM else DEAD) for x in xrange(SIZE)]) for x in xrange(SIZE)]
